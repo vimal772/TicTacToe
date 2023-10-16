a00ab3fb8b12a3a64483e93b7c40ea31b24a2f15
vimal772 <vimal115077286@gmail.com>
2023-10-16 18:52:41 +0530

    Ui changes hover fn added test case completed

--- script.js
+++ script.js
@@ -6,10 +6,27 @@ const O_Text = "O";
 const X_Text = "X";
 let currentPlayer = X_Text;
 let spaces = Array(9).fill(null);
+let count =0;
 
 const startGame = () => {
-    boxes.forEach((box) =>{
-        box.addEventListener('click',boxClicked)
+    boxes.forEach( box => {
+        box.addEventListener('mouseenter', () => {
+            if(!box.classList.contains('clicked')){
+                box.style.opacity = 0.5;
+                box.textContent = currentPlayer;
+            }
+        });
+    
+        box.addEventListener('mouseleave', () => {
+            if(!box.classList.contains('clicked')){
+                box.textContent = '';
+                box.style.opacity = 1;
+            }
+        });
+    
+        boxes.forEach((box) =>{
+            box.addEventListener('click',boxClicked)
+        });
     });
 }
 
@@ -18,17 +35,25 @@ function boxClicked(e){
 
     if(!spaces[id]){
         spaces[id] = currentPlayer;
+        e.target.classList.add('clicked');
+        e.target.style.opacity = 1;
         e.target.innerText = currentPlayer;
 
         if(playerHasWon() !== false){
-            playerText.innerText = `${currentPlayer} has Won`;
+            
             let winningBlocks = playerHasWon();
-
+            count = 1;
             winningBlocks.map(box => boxes[box].style.backgroundColor = `rgb(255,255,255)` )
              
         }
 
         currentPlayer = (currentPlayer == X_Text) ? O_Text:X_Text
+    }
+    if(spaces.every(element => element !== null) && count == 0){
+        playerText.textContent = "Match Draw";
+        setTimeout(() => {
+            restartClicked();
+        },1000);
     }
 }
 
@@ -48,7 +73,7 @@ function playerHasWon(){
         let [a,b,c] = condition;
 
         if(spaces[a] && (spaces[a] == spaces[b]) && (spaces[a] == spaces[c])){
-            
+            playerText.innerText = `${currentPlayer} has Won`;
             return [a,b,c];
         }
     }
@@ -59,13 +84,16 @@ restart.addEventListener('click',restartClicked);
 
 function restartClicked(){
     spaces.fill(null);
-
+    count = 0;
     boxes.forEach( box => {
         box.innerText = "";
         box.style.backgroundColor = '';
+        box.classList.remove('clicked');
     });
     playerText.innerText = "TIC TAC TOE"; 
     currentPlayer = X_Text;
 }
+
+
 
 startGame()
